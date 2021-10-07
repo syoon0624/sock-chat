b30c6cba07132e61cf9d5008580ce19402cd4a2d
seungyoonLee <syoon624@naver.com>
2021-10-07 17:37:25 +0900

    feat: csv route

--- routes/clubs.js
+++ routes/clubs.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const csv = require('../data');
 
 router.route('/').get((req, res, next) => {
-  return res.send('Club router works!');
+  return res.send('clubsRouter works!');
 });
 
 router.route('/:filename/').get((req, res) => {
   const fileName = req.params.filename;
-  res.send(fileName);
+  const filePath = path.join(__dirname, '..', 'data', fileName + '.csv');
+
+  const toSend = csv(filePath);
+  res.json(toSend);
 });
 
 module.exports = router;
