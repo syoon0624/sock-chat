2117b0a5426b1edaadaeed805595f3608a13f660
seungyoonLee <syoon624@naver.com>
2021-10-07 16:37:23 +0900

    feat: add router

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('Express work!');
-});
+const indexRouter = require('./routes/index');
+
+app.use('/', indexRouter);
 
 server.listen(3000, () => {
   console.log('Express Server is now listening on localhost:3000..');
