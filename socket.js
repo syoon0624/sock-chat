298620fc7c170a8faef2369fa6b850e4de093c90
seungyoonLee <syoon624@naver.com>
2021-10-08 17:15:09 +0900

    ??

--- socket.js
+++ socket.js
@@ -1,5 +1,13 @@
 module.exports = io => {
   io.on('connection', socket => {
     console.log('A user is now connected');
+
+    socket.on('chat message', msg => {
+      io.emit('chat message', msg);
+      console.log('msg: ', msg);
+    });
+    socket.on('disconnect', () => {
+      console.log('disconnected');
+    });
   });
 };
